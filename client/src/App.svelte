f9f27b781cf83ff2cb901135c6f5c734147dba29
zehnun <keldokuz@gmail.com>
2020-11-19 21:12:40 -0600

    fakjshdflkj

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,33 +1,11 @@
 <script>
-	export let name;
-	let title, text;
-
 	
-	function handleClick(){
-		alert('someone click the button')
-	}
-	function submitPost(){
-		alert('zakariya sucks')
-	}
 </script>
 
 
 
 <main>
-	<div class="bck">
-		<label class="header">Create a post </label>
-		<div class="submitTitle">
-			<label for="submitTitle">Title:</label>
-			<input class="Title" id="Title" type="text" bind:value={title}>
-		</div>
-	
-		<div class="submitText">
-			<label for="submitText">Text:</label>
-			<textarea id="submitText" type="text" bind:value={text}></textarea>
-			 <!-- <input id="submitText" type="text" bind:value={text}> -->
-		</div>
-		<button class="btn" on:click={submitPost} type="button">Submit</button>
-	</div>
+	<h1>Start here:</h1>
 
 </main>
 
@@ -36,37 +14,10 @@
 
 
 <style>
-	main{
+	h1{
 		display: flex;
 		align-items: center;
 		justify-content: center;
-	}
-	.header{
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		font-weight: bolder;
-		font-size: 50px;
-		margin-bottom: 20px;
-	}
-	.bck{
-		padding-top: 50px;
-	}
-	.Title{
-		width: 906px;
-	}
-	textarea { width: 906px; height: 608px; }
-	.submitTitle{
-		font-weight: bolder;
-		font-size: larger;
-	}
-	.submitText{
-		margin-top: 20px;
-		font-weight: bold;
-		font-size: larger;
-	}
-	.btn{
-		background-color: rgb(0, 197, 66);
-		color: white;
+		margin-top: 400px;
 	}
 </style>
