c4d6a6cbfd7a93a79a122279c84c8e954b1de373
zehnun <keldokuz@gmail.com>
2020-11-20 20:13:20 -0600

    fakjsfd

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,23 +1,39 @@
 <script>
-	
+	import { component_subscribe } from 'svelte/internal';
+import Home from './home.svelte';
+	import Nav from './navbar.svelte';
+
+	let Comment = [
+		{id: 1, author: "cool", body:"text"},
+		{id: 2, author: "cool7", body:"tedxt"},
+		{id: 3, author: "cool6", body:"tgext"},
+		{id: 4, author: "cool5", body:"tegsxt"},
+		{id: 5, author: "cool4", body:"tet"}
+	];
+
 </script>
 
 
-
 <main>
-	<h1>Start here:</h1>
+	{#each Comment as com, i}
+		{#if i % 2 == 0}
+			<div>
+				{com.author}
+				<div>
+					{com.body}
+				</div>
+			</div>
+	{/each}
 
 </main>
+<Nav></Nav>
+<Home></Home>
+
 
 
 
 
 
 <style>
-	h1{
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		margin-top: 400px;
-	}
+
 </style>
---
+++ client/src/createCom.svelte
@@ -0,0 +1,46 @@
+<script>
+    let com;
+</script>
+
+<main>
+    <div class="bck">
+		<label class="header">Post a comment</label>
+		<div class="submitCom">
+			<label for="submitCom">Comment:</label>
+			<textarea id="submitCom" type="text" bind:value={com}></textarea>
+		</div>
+		<button class="btn" on:click={submitPost} type="button">Submit</button>
+	</div>
+</main>
+
+<style>
+    main{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.header{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-weight: bolder;
+		font-size: 50px;
+		margin-bottom: 20px;
+	}
+	.bck{
+		padding-top: 50px;
+	}
+
+	textarea { width: 906px; height: 608px; }
+	.submitCom{
+		margin-top: 20px;
+		font-weight: bold;
+		font-size: larger;
+	}
+
+	.btn{
+		background-color: rgb(0, 197, 66);
+		color: white;
+	}
+</style>
+
